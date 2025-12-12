c2e1c4611c97fb0752d4795697051958fb30490d
revap292005-creator <revap292005@gmail.com>
2025-12-12 21:53:39 +0530

    assignment

---
+++ assignment/assignment1demo3.py
@@ -0,0 +1,13 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping the values
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
---
+++ assignment/assignment1demo5.py
@@ -0,0 +1,9 @@
+# Python program to print numbers from 1 to 20
+
+for i in range(1, 21):
+    print(i)
+# Using while loop
+i = 1
+while i <= 20:
+    print(i)
+    i += 1
