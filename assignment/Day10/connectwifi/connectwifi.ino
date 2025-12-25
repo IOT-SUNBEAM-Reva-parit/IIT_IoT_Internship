7a5e8c608b0ced93fb8a4b6a63ed02860cef5f64
revap292005-creator <revap292005@gmail.com>
2025-12-25 22:51:57 +0530

    assignment Day10 push

---
+++ assignment/Day10/connectwifi/connectwifi.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid = "Galaxy M06 5G 0152";
+const char* password = "reva292005";
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(ssid, password);
+
+  // Wait until connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWi-Fi connected successfully!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Nothing to do here
+}
